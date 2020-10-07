049a685d76ba91c8117976e4f0ab112f6a19c01d
LianYiMing <AGZX_lym@163.com>
2020-10-07 08:32:02 +0800

    优化js，内部转外部

---
+++ js/cat.js
@@ -0,0 +1,19 @@
+	L2Dwidget.init({
+				"pluginRootPath": "../js/live2dw/",
+				"pluginJsPath": "lib/",
+				"pluginModelPath": "assets/",
+				"tagMode": false,
+				"debug": false,
+				"model": {
+					"jsonPath": "../js/live2dw/assets/tororo.model.json"
+				},
+				"display": {
+					"position": "left",
+					"width": 100,
+					"height": 150
+				},
+				"mobile": {
+					"show": true
+				},
+				"log": false
+			});
