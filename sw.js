eec5dbe2a0aa02b7f4056450b16bdfd1017b312d
LianYiMing <AGZX_lym@163.com>
2020-10-07 11:43:42 +0800

    FIX footer的js名

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@ const PRECACHE_LIST = [
   "./offline.html",
   "./js/jquery.min.js",
   "./js/bootstrap.min.js",
-  "./js/hux-blog.min.js",
+  "./js/blog.min.js",
   "./js/snackbar.js",
   "./img/icon_wechat.png",
   "./img/avatar-hux.jpg",
